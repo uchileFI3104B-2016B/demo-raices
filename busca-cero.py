3474444907448e3d52fdcadbdf9ed3a1e8b69a30
Valentino Gonzalez <valentino.g.gonzalez@gmail.com>
2016-09-12 12:52:09 -0300

    plotea las funciones seno y coseno a intersectar

--- busca-cero.py
+++ busca-cero.py
@@ -4,3 +4,16 @@
 Este script busca el punto de interseccion entre seno y coseno
 (en el rango 0, pi) usando el metodo de la biseccion.
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x_values = np.linspace(0, np.pi, 100)
+plt.clf()
+plt.plot(x_values, np.sin(x_values), 'b', label='sin(x)')
+plt.plot(x_values, np.cos(x_values), 'r', label='cos(x)')
+plt.xlabel('x [radianes]')
+plt.legend()
+
+plt.show()
