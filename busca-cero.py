e1039e3f9ec10c67709a936bb489e7249bc0a650
Valentino Gonzalez <valentino.g.gonzalez@gmail.com>
2016-09-12 15:10:07 -0300

    Compara con implementacion de scipy

--- busca-cero.py
+++ busca-cero.py
@@ -7,14 +7,13 @@ Este script busca el punto de interseccion entre seno y coseno
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.optimize import bisect
 
 x_values = np.linspace(0, np.pi, 100)
 plt.clf()
 plt.plot(x_values, np.sin(x_values), 'b', label='sin(x)')
 plt.plot(x_values, np.cos(x_values), 'r', label='cos(x)')
 plt.xlabel('x [radianes]')
-plt.legend()
 
 
 def seno_menos_coseno(x):
@@ -43,9 +42,12 @@ def biseccion(func, a, b, itermax=100, tolerancia=1e-4):
 cero = biseccion(seno_menos_coseno, 0., 1.5, tolerancia=0.1)
 cero_mejor = biseccion(seno_menos_coseno, 0., 1.5, tolerancia=1e-5)
 
-plt.axvline(cero, color='g')
-plt.axvline(cero_mejor, color='k')
+cero_scipy = bisect(seno_menos_coseno, 0, 1.5, xtol=1e-5)
 
+#plt.axvline(cero, color='g')
+plt.axvline(cero_mejor, color='k', label='esta implementacion')
+plt.axvline(cero_scipy, color='c', label='scipy')
 
-
-plt.show()
+plt.legend()
+# plt.show()
+plt.savefig('raiz.png')
