dcdd6634428f274540920562bcb39bd1baa66c4c
Valentino Gonzalez <valentino.g.gonzalez@gmail.com>
2016-09-12 13:18:38 -0300

    implementa biseccion con counter

--- busca-cero.py
+++ busca-cero.py
@@ -16,8 +16,33 @@ plt.plot(x_values, np.cos(x_values), 'r', label='cos(x)')
 plt.xlabel('x [radianes]')
 plt.legend()
 
-plt.show()
 
 def seno_menos_coseno(x):
     output = np.sin(x) - np.cos(x)
     return output
+
+def biseccion(func, a, b, numero_de_iteraciones=10):
+    """
+    Busca el cero de la funcion func, ubicado entre a y b
+    usando el metodo de la biseccion.
+    """
+    p = (a + b) / 2
+    counter = 0
+    while counter < numero_de_iteraciones:
+        if func(p) == 0:
+            return p
+        if func(a) * func(p) > 0:
+            a = p
+        else:
+            b = p
+        p = (a + b) / 2
+        counter += 1
+    return p
+
+
+cero = biseccion(seno_menos_coseno, 0., 1.5, numero_de_iteraciones=10)
+
+plt.axvline(cero, color='g')
+
+
+plt.show()
